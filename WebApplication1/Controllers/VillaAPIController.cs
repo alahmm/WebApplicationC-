de5e496e7664b40bd11964139c2c6bf181f921fe
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-08-28 11:24:25 +0200

    Add project files.

---
+++ WebApplication1/Controllers/VillaAPIController.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Logging;
+using WebApplication1.Models;
+using WebApplication1.Models.Dto;
+
+namespace WebApplication1.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]//activate the validation
+    public class VillaAPIController : ControllerBase
+    {
+        //private readonly ILogger<VillaAPIController> _logger;
+
+        //public VillaAPIController(ILogger<VillaAPIController> logger)//write ctor + tab twice.    strg + . to have more options about parameter
+        //{
+        //    _logger = logger; //here the standard logger will be used
+        //}
+        private readonly ILogging _logger;
+        public VillaAPIController(ILogging logger) {
+
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            _logger.Log("getting all villas", "");
+            return Ok(VillaStore.villas);
+        }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]//or {id:int}
+                                          //[ProducesResponseType(200), Type = typeof(VillaDTO)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                _logger.Log("id error with id " + id, "error");
+                return BadRequest();
+            }
+            var villa = VillaStore.villas.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> createVilla([FromBody] VillaDTO villaDTO)
+        {
+            //if (!ModelState.IsValid)//this validation can be added in case we do not have [apiController] at the beginning
+            //{
+            //    return BadRequest(ModelState);
+            //}
+            if (VillaStore.villas.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("customError", "Villa already exists");//
+                return BadRequest(ModelState);
+            }
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villas.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villas.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);// this returns 201 or just ok(object)
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villas.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villas.Remove(villa);
+            return NoContent();
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult updateVilla(int id, [FromBody] VillaDTO villaDTO)
+        {
+
+            if (villaDTO == null || villaDTO.Id != id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villas.FirstOrDefault(u => u.Id == id);
+            villa.Name = villaDTO.Name;
+            villa.Occupancy = villaDTO.Occupancy;
+            villa.Sqft = villaDTO.Sqft;
+            return NoContent();
+
+        }
+
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult updatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+        {
+            if (patchDTO == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villas.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            patchDTO.ApplyTo(villa, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return NoContent();
+        }
+
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,44 @@
+
+using WebApplication1.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Log.Logger = new LoggerConfiguration()  //using serilog for Log
+//    .MinimumLevel.Debug()
+//    .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();      //use serilog to custom default logger
+
+//builder.Host.UseSerilog();//to say to the app to use serilog,
+//now i will not need it later so i will uninstall iSerilog.AspNetCore and Serilog.Sinks.File from app manager
+
+builder.Services
+    .AddControllers(option => {
+        option.ReturnHttpNotAcceptable = true;
+    })//inside of addcontrollers you can add this parameter option => { option.ReturnHttpNotAcceptable = true;}
+    .AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();//add the dependency to package that we added to project//options zo specify return type//serializer o add xml serializer to api
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//to register a service which provide an object for the implementation of ilogging
+builder.Services.AddSingleton<ILogging, LoggingV2>();
+   // by using singleton, one logger will be used through up the application
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
