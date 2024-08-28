9f00cc6e759c1b704969c9f96d3c6001eeb8fb3f
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-08-28 12:01:15 +0200

    delete comments about using serilog

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,13 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//Log.Logger = new LoggerConfiguration()  //using serilog for Log
-//    .MinimumLevel.Debug()
-//    .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();      //use serilog to custom default logger
-
-//builder.Host.UseSerilog();//to say to the app to use serilog,
-//now i will not need it later so i will uninstall iSerilog.AspNetCore and Serilog.Sinks.File from app manager
-
 builder.Services
     .AddControllers(option => {
         option.ReturnHttpNotAcceptable = true;
