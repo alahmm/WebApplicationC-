17ad54ab1fee0562097232ed4b0c896a73de61a8
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-08-29 12:09:30 +0200

    Connect entity to table of database

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace WebApplication1.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<Villa> Villas { get; set; } //that will be the name of the table
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }//then navigate to tools => nuget package manager=> type in console add-migration AddVillaTable
+        public DbSet<Villa> Villas { get; set; } //that will be the name of the table=> then to apply these migrations run the command update-database
     }
 }
