98eab65f16682c50d27b88e3768d010b5411869c
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-09-12 12:46:37 +0200

    Add view/action controller for registration/login

---
+++ MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿
+using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaurl;
+        public AuthService(IHttpClientFactory httpClient, IConfiguration configuration) : base(httpClient)
+        {
+            _clientFactory = httpClient;
+            villaurl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaurl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegistrationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaurl + "/api/UsersAuth/register"
+            });
+        }
+    }
+}
